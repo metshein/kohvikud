17adc091e5188248e39aba1dd255bcf101d5a94f
Mario Metshein <mario.metshein@hkhk.edu.ee>
2024-05-22 08:42:39 +0300

    sf

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Kohvikud</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
@@ -43,6 +43,19 @@
     ?>
     <div class="container">
         <h1>Valige asutus mida hinnata</h1>
+        <div class="row">
+            <div class="col-9">
+            </div>
+            <div class="col-3 text-end">
+            <form class="mb-3">
+            <div class="input-group">
+                <input type="text" class="form-control" name="s" placeholder="Otsi asutust" value="<?php if (!empty($_GET["s"])) echo $_GET["s"]; ?>">
+                <button class="btn btn-primary" type="submit">Otsi</button>
+            </div>
+        </form>
+            </div>
+        </div>
+
         <table class="table table-sm">
             <tr>
                 <th>Nimi</th>
